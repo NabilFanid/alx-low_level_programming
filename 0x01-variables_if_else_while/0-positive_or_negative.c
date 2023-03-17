2e79099cd74a13c8c5727b0a36c334ee01550c9b
nabil <nabilfanid19@gmail.com>
2023-03-17 15:56:40 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include
+
+int main ()
+ {
+  if (n >= 0)
+      if (n > 0)
+      printf("positive : %d \n")
+      else
+      printf("is zero  : %d \n")
+  else
+  printf("is negative : %d \n")
+    
+ }
